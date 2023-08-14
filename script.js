fee06dc16eda7380192356c1bc43a3c49571af94
Tiago Santos <tiagosanalvess@gmail.com>
2023-08-14 11:31:45 -0300

    Refactoring this mess up!
    I splited some processes into functions to improve readability and  added a function to clear the inputs.

--- script.js
+++ script.js
@@ -1,71 +1,67 @@
 function calculate() {
-    const date = new Date();
-    const year = date.getFullYear();
-    const formsage = document.getElementById("age");
-    const res = document.querySelector("div#res");
-    const birth = Number(formsage.value);
-    const age = year - birth;
-    const fsex = document.getElementsByName("radsex");
-    let sex = "";
-    const img = document.getElementById("image");
+    const date = new Date()
+    const year = date.getFullYear()
+    const formsage = document.getElementById("age")
+    const res = document.querySelector("div#res")
+    const birth = Number(formsage.value)
+    const age = year - birth
+    const fsex = document.getElementsByName("radsex")
+    let sex = ""
+    const img = document.getElementById("image")
     
     if (formsage.value.length < 4 || birth >= year || (fsex[0].checked == false && fsex[1].checked == false)) {
-        alert("[ERRO] Verifique os dados e tente novamente");
+        alert("[ERRO] Verifique os dados e tente novamente")
         clearInputs()
-        return;
+        return
     }
     
     if (fsex[0].checked) {
         sex = "homem";
-        itsMale(img, age);
+        itsMale(img, age)
     } else if (fsex[1].checked) {
-        sex = "mulher";
-        itsFemale(img, age);
+        sex = "mulher"
+        itsFemale(img, age)
     }
     
-    res.style.textAlign = "center";
-    res.innerHTML = `Detectado ${sex} de ${age} anos`;
+    res.style.textAlign = "center"
+    res.innerHTML = `Detectado ${sex} de ${age} anos`
     clearInputs()
 }
 
 function itsMale(img, age) {
     if (age >= 0 && age <= 2) {
-        img.src = "./assets/babyboy.png";
+        img.src = "./assets/babyboy.png"
     } else if (age >= 3 && age < 13) {
-        img.src = "./assets/childboy.png";
+        img.src = "./assets/childboy.png"
     } else if (age < 25) {
-        img.src = "./assets/teenboy.png";
+        img.src = "./assets/teenboy.png"
     } else if (age < 35) {
-        img.src = "./assets/youngadultman.png";
+        img.src = "./assets/youngadultman.png"
     } else if (age < 60) {
-        img.src = "./assets/adultman.png";
+        img.src = "./assets/adultman.png"
     } else {
-        img.src = "./assets/elderman.png";
+        img.src = "./assets/elderman.png"
     }
 }
 
 function itsFemale(img, age) {
     if (age >= 0 && age < 2) {
-        img.src = "./assets/babygirl.png";
+        img.src = "./assets/babygirl.png"
     } else if (age >= 3 && age < 13) {
-        img.src = "./assets/childgirl.png";
+        img.src = "./assets/childgirl.png"
     } else if (age < 25) {
-        img.src = "./assets/teengirl.png";
-    } else if (age < 35) {
-        img.src = "./assets/youngadultwoman.png";
+        img.src = "./assets/teengirl.png"
+        img.src = "./assets/youngadultwoman.png"
     } else if (age < 60) {
-        img.src = "./assets/adultwoman.png";
+        img.src = "./assets/adultwoman.png"
     } else {
-        img.src = "./assets/elderwoman.png";
+        img.src = "./assets/elderwoman.png"
     }
 }
 
 function clearInputs() {
-    const formsage = document.getElementById("age");
-    const fsex = document.getElementsByName("radsex");
-    const res = document.querySelector("div#res");
-    const img = document.getElementById("image");
-    
-    formsage.value = "";
-    fsex.forEach(radio => (radio.checked = false));
+    const formsage = document.getElementById("age")
+    const fsex = document.getElementsByName("radsex")
+    formsage.value = ""
+    fsex.forEach(radio => (radio.checked = false))
 }
