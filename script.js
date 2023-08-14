134b1e38ad47f307b660c8b08f8cc394ab2d2968
Tiago Santos <tiagosanalvess@gmail.com>
2023-08-14 11:25:56 -0300

    Refactoring this mess up!

--- script.js
+++ script.js
@@ -1,77 +1,71 @@
 function calculate() {
-    const date = new Date()
-    const year = date.getFullYear()
-    let formsage = document.getElementById("age")
-    let res = document.querySelector("div#res")
-    let birth = Number(formsage.value)
-    let age = (year - birth)
-    if (formsage.value.length < 4 || birth > year) {
-        alert("[ERRO] Verifique os dados e tente novamente")
-        pause
-    }
-    let fsex = document.getElementsByName("radsex")
-    let sex = ''
-    let img = document.getElementById("image")
+    const date = new Date();
+    const year = date.getFullYear();
+    const formsage = document.getElementById("age");
+    const res = document.querySelector("div#res");
+    const birth = Number(formsage.value);
+    const age = year - birth;
+    const fsex = document.getElementsByName("radsex");
+    let sex = "";
+    const img = document.getElementById("image");
     
-    if (fsex[0].checked == false && fsex[1].checked == false) {
-        alert("[ERRO] Verifique os dados e tente novamente")
+    if (formsage.value.length < 4 || birth >= year || (fsex[0].checked == false && fsex[1].checked == false)) {
+        alert("[ERRO] Verifique os dados e tente novamente");
+        clearInputs()
+        return;
     }
-    else {
+    
     if (fsex[0].checked) {
-        sex = 'homem'
-        //Baby
-        if (age >= 0 && age <= 2) {
-            img.src = './assets/babyboy.png'
-        }
-        //Child
-        else if (age >= 3 && age < 13) {
-            img.src = './assets/childboy.png'
-        }
-        //Teen
-        else if (age < 25) {
-            img.src = './assets/teenboy.png'
-        }
-        //Young adult
-        else if (age < 35) {
-            img.src = './assets/youngadultman.png'
-        }
-        //Adult
-        else if (age < 60) {
-            img.src = './assets/adultman.png'
-        }
-        //Elder
-        else {
-            img.src = './assets/elderman.png'
-        }
+        sex = "homem";
+        itsMale(img, age);
+    } else if (fsex[1].checked) {
+        sex = "mulher";
+        itsFemale(img, age);
     }
-    if (fsex[1].checked) {
-        sex = 'mulher'
-        //Baby
-        if (age >= 0 && age < 2) {
-            img.src = './assets/babygirl.png'
-        }
-        //Child
-        else if (age >= 3 && age < 13) {
-            img.src = './assets/childgirl.png'
-        }
-        //Teen
-        else if (age < 25) {
-            img.src = './assets/teengirl.png'
-        }
-        //Young adult
-        else if (age < 35) {
-            img.src = './assets/youngadultwoman.png'
-        }
-        //Adult
-        else if (age < 60) {
-            img.src = './assets/adultwoman.png'
-        }
-        //Elder
-        else if (age < 80) {
-            img.src = './assets/elderwoman.png'
-        }
+    
+    res.style.textAlign = "center";
+    res.innerHTML = `Detectado ${sex} de ${age} anos`;
+    clearInputs()
+}
+
+function itsMale(img, age) {
+    if (age >= 0 && age <= 2) {
+        img.src = "./assets/babyboy.png";
+    } else if (age >= 3 && age < 13) {
+        img.src = "./assets/childboy.png";
+    } else if (age < 25) {
+        img.src = "./assets/teenboy.png";
+    } else if (age < 35) {
+        img.src = "./assets/youngadultman.png";
+    } else if (age < 60) {
+        img.src = "./assets/adultman.png";
+    } else {
+        img.src = "./assets/elderman.png";
     }
-    res.style.textAllign = 'center'
-    res.innerHTML = `Detectado ${sex} de ${age} anos`
+}
+
+function itsFemale(img, age) {
+    if (age >= 0 && age < 2) {
+        img.src = "./assets/babygirl.png";
+    } else if (age >= 3 && age < 13) {
+        img.src = "./assets/childgirl.png";
+    } else if (age < 25) {
+        img.src = "./assets/teengirl.png";
+    } else if (age < 35) {
+        img.src = "./assets/youngadultwoman.png";
+    } else if (age < 60) {
+        img.src = "./assets/adultwoman.png";
+    } else {
+        img.src = "./assets/elderwoman.png";
     }
+}
+
+function clearInputs() {
+    const formsage = document.getElementById("age");
+    const fsex = document.getElementsByName("radsex");
+    const res = document.querySelector("div#res");
+    const img = document.getElementById("image");
+    
+    formsage.value = "";
+    fsex.forEach(radio => (radio.checked = false));
 }
